bdd7248e7e00d9155585e160975d281a4439bf12
deruip <aronly1997@hotmail.com>
2018-10-30 20:06:42 -0400

    Added ASP pages, scripts
    Login + registration pages, database scripts

---
+++ BLAIR1/BLAIR1/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    public void LoginMethod(object sender, EventArgs e)
+    {
+        //TODO
+        //create code to interact with database
+    }
+    //redirects to the registration page
+    protected void ToRegisterPage(object sender, EventArgs e)
+    {
+        Response.Redirect("Page_Registration.aspx");
+    }
+}
