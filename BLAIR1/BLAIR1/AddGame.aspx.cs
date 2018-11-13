f1407b2a31bd9006469be978c6375a175c3969c9
admiralkuro <broyouchill@gmail.com>
2018-11-13 17:41:22 -0500

    Added files from Lance-Branch
    included a bin folder to BLAIR1

--- BLAIR1/BLAIR1/AddGame.aspx.cs
+++ BLAIR1/BLAIR1/AddGame.aspx.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Collections.Generic;
-using System.Linq;
+using System.Data.SqlClient;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
